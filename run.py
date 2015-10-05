118b96345cbf93e6f72d89af3843ee3379722c2c
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-10-04 22:06:31 -0500

    send email proff

--- run.py
+++ run.py
@@ -1,9 +1,33 @@
 from flask import Flask
-app = Flask(__name__)
- 
-@app.route('/')
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+
+app =Flask(__name__)
+mail=Mail(app)
+
+app.config.update(
+	DEBUG= True,
+	MAIL_SERVER= 'smtp.gmail.com',
+	MAIL_PORT= 587,
+	MAIL_USE_SSL= False,
+	MAIL_USE_TLS= True,
+	MAIL_USERNAME = 'eniqqo8181@gmail.com',
+	MAIL_PASSWORD = 'AIKOnikolas'
+	)
+
+mail=Mail(app)
+
+@app.route("/")
+def index():
+	msg = Message(
+              'Hello',
+	       sender='eniqqo8181@gmail.com',
+	       recipients=
+               ['eniqqo8181@gmail.com'])
+	msg.body = "This is the email body"
+	mail.send(msg)
+	
 def hello_world():
     return 'Hola Profesor Jorge Neyra!'
- 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
